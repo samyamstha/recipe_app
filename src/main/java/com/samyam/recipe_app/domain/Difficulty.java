535f38525f43f56ea58b7af4b62a36846647ce21
samyamshrestha <samyamsth@gmail.com>
2018-06-09 02:02:39 -0400

    Added domain classes with JPA Relationships

---
+++ src/main/java/com/samyam/recipe_app/domain/Difficulty.java
@@ -0,0 +1,4 @@
+package com.samyam.recipe_app.domain;
+
+public enum Difficulty {
+}
---
+++ src/main/java/com/samyam/recipe_app/domain/Recipe.java
@@ -0,0 +1,115 @@
+package com.samyam.recipe_app.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private int prepTime;
+    private int cookTime;
+    private  int servings;
+    private  String source;
+    private String url;
+    private String directions;
+    private Difficulty difficulty;
+
+    @Lob
+    private Byte[] Image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Byte[] getImage() {
+        return Image;
+    }
+
+    public void setImage(Byte[] image) {
+        Image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+}
