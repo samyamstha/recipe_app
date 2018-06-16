b706677752b150200baa565ba456d167e1013490
samyamshrestha <samyamsth@gmail.com>
2018-06-15 20:54:02 -0400

    Created Unit Tests for category class.

---
+++ src/test/java/com/samyam/recipe_app/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.samyam.recipe_app.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+    }
+}
