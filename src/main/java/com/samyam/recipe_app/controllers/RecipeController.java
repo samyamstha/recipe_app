a2fecf5eb6349382ef01ed7f9320df87e6f879ec
samyamshrestha <samyamsth@gmail.com>
2018-06-22 00:56:18 -0400

    Added Update Recipe functionality.

--- src/main/java/com/samyam/recipe_app/controllers/RecipeController.java
+++ src/main/java/com/samyam/recipe_app/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.samyam.recipe_app.controllers;
 
+import com.samyam.recipe_app.commands.RecipeCommand;
 import com.samyam.recipe_app.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -19,12 +22,33 @@ public class RecipeController {
     }
 
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+
     }
 
 
