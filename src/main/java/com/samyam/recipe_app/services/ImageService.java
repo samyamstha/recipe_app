c99e1214d8292135e7230560781fcdb8c1ebf4da
samyamshrestha <samyamsth@gmail.com>
2018-06-28 00:56:52 -0400

    Added upload image functionality.

---
+++ src/main/java/com/samyam/recipe_app/services/ImageService.java
@@ -0,0 +1,9 @@
+package com.samyam.recipe_app.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
